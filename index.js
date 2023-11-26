0904bf642d51bd84001f4960245273a4d4bf6ec6
ajuala <kamalustudio@yandex.com>
2023-11-26 11:31:29 +0100

    pdfkit test, attempt 4

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-
+const PDFDocument = require("pdfkit");
+const streamBuffers = require("stream-buffers");
 
 
 export default {
 	fetch() {
 
-		return new Response("Happy people", {
+		let doc = new PDFDocument();
+		let buf = new streamBuffers.WritableStreamBuffer();
+		doc.pipe(buf);
+
+		doc.text("When the winter comes and the white winds blow, the lone wolf dies but the pack survives.");
+		doc.end();
+
+		return new Response(buf.getContents(), {
 			headers: {
-				'content-type': 'plain/text',
+				'content-type': 'application/pdf',
+				'content-disposition': 'attachment; filename="the-north-remembers.pdf"',
 			},
 		});
 	},
