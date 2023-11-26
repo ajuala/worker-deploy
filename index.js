6a93555a111f3adf71f5e406b70a39e05a4f17c8
ajuala <kamalustudio@yandex.com>
2023-11-26 06:10:00 +0100

    another edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
 	fetch() {
-		return new Response(new Date().toISOString(), {
+		return new Response(new Date().toISOString().slice(0, 10), {
 			headers: {
 				'content-type': 'text/plain',
 			},
