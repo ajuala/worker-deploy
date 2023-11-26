ec119750e8bcb8f2b45109639d21c37083138320
ajuala <kamalustudio@yandex.com>
2023-11-26 14:25:07 +0100

    fauna test, attempt 17

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ export default {
 		try {
 			let client = new Client({secret: env.FAUNA_SECRET});
 			let q1 = await client.query(fql`Time.now()`);
-			let q3 = await client.query(fql`Time.now()`);
 
 		} catch(err) {
 			console.log(err);
 			return new Response(err.toString());
 		}
-		return new Response(JSON.stringify([q1, q3]), {
+		return new Response(JSON.stringify(q1), {
 			headers: {
 				'content-type': 'application/json',
 			},
