d670385ff56ebbe27cf644490e1f80d1762e23e1
ajuala <kamalustudio@yandex.com>
2023-11-26 13:18:49 +0100

    fauna test, attempt 6

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const { fql, Client, FaunaError } = require('fauna');
 
 export default {
 	async fetch(req, env) {
-		let client = new Client({secret: env.FAUNA_SECRET});
-		let q = await client.query(fql`Time.now()`);
 
+		try {
+			let client = new Client({secret: env.FAUNA_SECRET});
+			let q = await client.query(fql`Time.now()`);
 
+		} catch(err) {
+			console.log(err);
+		}
 		return new Response(JSON.parse(q), {
 			headers: {
 				'content-type': 'application/json',
